3d1af08d8392fbd03e10b5d7e492368d095404cf
Arthur Bied-Charreton <arthurbiedchar@gmail.com>
2025-06-28 21:26:10 +0200

    Add decryption routine to asm stub

--- stub/test.asm
+++ stub/test.asm
@@ -1,4 +1,14 @@
+ENTRYPOINT_MARKER: equ 0x4242424242424242
+MPROTECT_SIZE_MARKER: equ 0x2424242424242424
+MPROTECT_ADDR_MARKER: equ 0x6969696969696969
+
 SYS_WRITE: equ 1
+SYS_MPROTECT: equ 10
+SYS_EXIT: equ 60
+
+PROT_READ: equ 0x01
+PROT_WRITE: equ 0x02
+PROT_EXEC: equ 0x04
 
 _start:
     push rax
@@ -24,5 +34,65 @@ get_string_addr:
     pop rdi
     pop rax
 
-    mov rax, 0x4242424242424242
+    call start_decryption
+
+start_decryption:
+    push rax
+    push rdi
+    push rsi
+    push rdx
+
+    mov rax, SYS_MPROTECT
+    mov rdi, MPROTECT_ADDR_MARKER
+    mov rsi, MPROTECT_SIZE_MARKER
+    mov rdx, PROT_READ | PROT_WRITE | PROT_EXEC
+    syscall
+
+    test rax, rax
+    js error
+
+    pop rdx
+    pop rsi
+    pop rdi
+    pop rax
+
+    push rax
+    push rdi
+    push rsi
+    push rdx
+    push r8
+    push r9
+
+    mov rsi, MPROTECT_ADDR_MARKER
+    mov rcx, MPROTECT_SIZE_MARKER
+    mov rdx, 0 ; counter
+
+xor_loop:
+    cmp rdx, rcx
+    jge call_original_code
+
+    mov r9b, [rsi + rdx]
+
+    xor r9b, 0x01
+    xor r9b, 0x01
+
+    mov [rsi + rdx], r9b
+    inc rdx
+
+    jmp xor_loop
+
+call_original_code:
+    pop r9
+    pop r8
+    pop rdx
+    pop rsi
+    pop rdi
+    pop rax
+
+    mov rax, ENTRYPOINT_MARKER
     jmp rax
+
+error:
+    mov rax, SYS_EXIT
+    mov rdi, 1
+    syscall
