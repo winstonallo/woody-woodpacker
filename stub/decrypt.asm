091cdbb7f69429d05174a6223fff222e30751f6a
Arthur Bied-Charreton <arthurbiedchar@gmail.com>
2025-07-04 20:39:47 +0200

    Rename test.asm to decrypt.asm

--- stub/decrypt.asm
+++ stub/decrypt.asm
@@ -1,117 +1,110 @@
-struc WoodyData
-    .text_entrypoint:   resq 1  ; 1x8 bytes
-    .text_size:         resq 1  ; 1x8 bytes
-    .key:               resb 16 ; 16x1 bytes
-endstruc
+ENTRYPOINT_MARKER: equ 0x4242424242424242
+MPROTECT_SIZE_MARKER: equ 0x2424242424242424
+MPROTECT_ADDR_MARKER: equ 0x6969696969696969
+DECRYPT_START_OFFSET_MARKER: equ 0x6666666666666666
+DECRYPT_LEN_MARKER: equ 0x3333333333333333
 
+SYS_WRITE: equ 1
+SYS_MPROTECT: equ 10
+SYS_EXIT: equ 60
 
-; syscalls
-SYS_WRITE:      equ 1
-SYS_MPROTECT:   equ 10
-SYS_EXIT:       equ 60
-
-; mprotect flags
-PROT_READ:      equ 0x1
-PROT_WRITE:     equ 0x2
-PROT_EXEC:      equ 0x4
+PROT_READ: equ 0x01
+PROT_WRITE: equ 0x02
+PROT_EXEC: equ 0x04
 
 _start:
     push rax
-    push r8
     push rdi
     push rsi
     push rdx
-    push rcx
-    call call_original_code
 
-print_woody:
     mov rax, SYS_WRITE
     mov rdi, 1
-    mov rsi, WOODY
-    mov rdx, WOODY_LEN
-    syscall
-    ret
 
-decrypt:
+    call get_string_addr
+
+woody_string:
+    db '....WOODY....', 10
+
+xor_key:
+    db 0x01, 0x01, 0x01, 0x01, 0x01, 0x01, 0x01, 0x01, 0x01, 0x01, 0x01, 0x01, 0x01, 0x01, 0x01, 0x01
+
+get_string_addr:
+    pop rsi
+    mov rdx, 14
+    syscall
+
+    pop rdx
+    pop rsi
+    pop rdi
+    pop rax
+
+    call start_decryption
+
+start_decryption:
+    push rax
+    push rdi
+    push rsi
+    push rdx
+
     mov rax, SYS_MPROTECT
-    mov rdi, [WOODY_DATA + WoodyData.text_entrypoint]
-    mov rsi, [WOODY_DATA + WoodyData.text_size]
-    mov rdx, PROT_WRITE
-    or rdx, PROT_READ
+    mov rdi, MPROTECT_ADDR_MARKER
+    mov rsi, MPROTECT_SIZE_MARKER
+    mov rdx, PROT_READ | PROT_WRITE | PROT_EXEC
     syscall
 
     test rax, rax
     js error
 
-    mov rsi, [WOODY_DATA + WoodyData.text_entrypoint]
-    mov rcx, [WOODY_DATA + WoodyData.text_size]
-    mov rdx, 0  ; counter
-    mov r8, 0   ; key index
-
-xor_loop:
-    ; if (counter >= text_size) { ... }
-    cmp rdx, rcx
-    jge decrypt_done
-
-    ; uint8_t byte_to_encrypt = text[*rdx]
-    mov al, [rsi + rdx]
-
-    ; uint8_t key_byte = key[*r8]
-    mov dl, [WOODY_DATA + WoodyData.key + r8]
-
-    ; byte_to_encrypt ^= key_byte
-    xor al, dl
-
-    ; text[*rdx] = byte_to_encrypt
-    mov [rsi + rdx], al
-    inc rdx
-    inc r8
-
-    cmp r8, 16
-    jl xor_loop
-    mov r8, 0
-    jmp xor_loop
-
-decrypt_done:
-    mov rax, SYS_MPROTECT
-    mov rdi, [WOODY_DATA + WoodyData.text_entrypoint]
-    mov rsi, [WOODY_DATA + WoodyData.text_size]
-    mov rdx, PROT_READ
-    or rdx, PROT_EXEC
-    syscall
-
-    ret
-
-call_original_code:
-    pop rcx
     pop rdx
     pop rsi
     pop rdi
     pop rax
+
+    push rax
+    push rdi
+    push rsi
+    push rdx
+    push r8
+    push r9
+    push r10
+    push r11
+
+    lea r11, [rel xor_key]
+    mov rsi, DECRYPT_START_OFFSET_MARKER
+    mov rcx, DECRYPT_LEN_MARKER
+    mov rdx, 0 ; counter
+    mov r8, 0
+
+xor_loop:
+    cmp rdx, rcx
+    jge call_original_code
+
+    mov r9b, [rsi + rdx]
+    mov r10b, [r11 + r8]
+
+    xor r9b, r10b
+    mov [rsi + rdx], r9b
+
+    inc rdx
+    inc r8
+    and r8, 0xf
+    jmp xor_loop
+
+call_original_code:
+    pop r11
+    pop r10
+    pop r9
     pop r8
-    mov rax, qword 0x401020
+    pop rdx
+    pop rsi
+    pop rdi
+    pop rax
+
+    mov rax, ENTRYPOINT_MARKER
     jmp rax
 
 error:
-    mov rax, SYS_WRITE
-    mov rdi, 2
-    mov rsi, ERROR_MSG
-    mov rdx, ERROR_MSG_LEN
-    syscall
-
     mov rax, SYS_EXIT
     mov rdi, 1
     syscall
-
-
-
-ERROR_MSG:      db 'Error', 10
-ERROR_MSG_LEN:  equ $-ERROR_MSG
-
-WOODY:          db '....WOODY....', 10
-WOODY_LEN:      equ $-WOODY
-
-WOODY_DATA:
-    dq 0xdeadbeefcafebabe   ; .text_entrypoint
-    dq 0xcafebabedeadbeef   ; .text_size
-    times 16 db 0x42        ; .key
