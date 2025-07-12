1564efc3791b881190accf42222ce00b12f42598
Felix Brüggemann <57171559+FelixBrgm@users.noreply.github.com>
2025-07-12 15:35:04 +0000

    refactored from fd to mmap for finding

--- src/inc/woody.h
+++ src/inc/woody.h
@@ -11,14 +11,20 @@ typedef struct {
     uint64_t size;
 } code_cave_t;
 
+typedef struct {
+    void *mem;
+    size_t size;
+} file;
+
 int elf64_ident_check(const Elf64_Ehdr *header);
 int key_create(int ac, char **av, u_int8_t *key);
 int file_duplicate(char **av);
-int elf_header_parse(int fd, Elf64_Ehdr *header);
-int section_header_entry_get(const Elf64_Ehdr header, Elf64_Shdr *section_header_entry, int fd);
-int program_header_by_section_header_get(const Elf64_Ehdr header, const Elf64_Shdr section_header, Elf64_Phdr *program_header_res, int fd);
-int program_header_get_next(const Elf64_Ehdr header, const Elf64_Phdr program_header_cur, Elf64_Phdr *program_header_next, int fd);
-int code_cave_get(const Elf64_Ehdr header, const uint64_t ph_start, const uint64_t ph_end, code_cave_t *code_cave, int fd);
+const Elf64_Shdr *section_header_entry_get(file file, const Elf64_Ehdr header);
+int elf64_ident_check(const Elf64_Ehdr *header);
+const Elf64_Phdr *program_header_by_section_header_get(file file, const Elf64_Ehdr header, const Elf64_Shdr section_header);
+const Elf64_Phdr *program_header_get_after(file file, const Elf64_Ehdr header, const Elf64_Phdr program_header);
+int code_cave_get(file file, code_cave_t *code_cave, const Elf64_Ehdr header, const uint64_t start, const uint64_t end);
+
 int shellcode_inject(Elf64_Ehdr header, Elf64_Phdr program_header, const code_cave_t code_cave, uint8_t shellcode[], const uint64_t shellcode_size, int fd,
                      const uint8_t key[16], const uint64_t encryption_start, const uint64_t encryption_size);
 int section_text_encrypt(const Elf64_Shdr section_header_entry, int fd, const uint8_t key[16]);
--- src/main.c
+++ src/main.c
@@ -13,6 +13,43 @@
 #include <unistd.h>
 
 int
+file_mmap(const char *file_name, file *file) {
+    assert(file_name != NULL);
+    assert(file != NULL);
+
+    int fd = open(file_name, O_RDONLY);
+    if (fd == -1) {
+        perror(file_name);
+        return -1;
+    }
+
+    int off = lseek(fd, 0, SEEK_END);
+    if (off == -1) {
+        close(fd);
+        perror(file_name);
+        return -1;
+    }
+    file->size = off;
+
+    off = lseek(fd, 0, SEEK_SET);
+    if (off != 0) {
+        close(fd);
+        perror(file_name);
+        return -1;
+    }
+
+    file->mem = mmap(NULL, file->size, PROT_READ | PROT_WRITE, MAP_PRIVATE, fd, 0);
+    close(fd);
+
+    if (file->mem == NULL) {
+        perror("mem");
+        return -1;
+    }
+
+    return 0;
+}
+
+int
 main(int ac, char **av) {
     if (ac != 2 && ac != 3) {
         printf("wrong usage: ./woody BINARY_NAME [ENCRYPTION_KEY(16 bytes)]\n");
@@ -22,51 +59,44 @@ main(int ac, char **av) {
     const uint8_t key[16] = {0};
     if (key_create(ac, av, (u_int8_t *)key)) return 1;
 
-    int file = file_duplicate(av);
-    if (file == -1) return 1;
+    file file;
+    const char *binary_name = av[1];
+    if (file_mmap(binary_name, &file)) return 1;
 
-    Elf64_Ehdr header;
-    if (elf_header_parse(file, &header)) {
-        close(file);
-        return 1;
-    }
+    Elf64_Ehdr *header = file.mem;
 
-    Elf64_Shdr section_header_entry;
+    if (elf64_ident_check(header)) return 1;
 
-    if (section_header_entry_get(header, &section_header_entry, file)) {
-        close(file);
-        return 1;
-    }
-    Elf64_Phdr program_header_entry;
-    if (program_header_by_section_header_get(header, section_header_entry, &program_header_entry, file)) {
-        close(file);
-        return 1;
-    }
+    const Elf64_Shdr *section_header_entry = section_header_entry_get(file, *header);
+    if (section_header_entry == NULL) return 1;
+    printf("Found section header which includes the entry point: 0x%lx - 0x%lx\n", section_header_entry->sh_offset,
+           section_header_entry->sh_offset + section_header_entry->sh_size);
 
-    Elf64_Phdr program_header_entry_next;
-    if (program_header_get_next(header, program_header_entry, &program_header_entry_next, file)) {
-        close(file);
-        return 1;
-    }
+    const Elf64_Phdr *program_header_entry = program_header_by_section_header_get(file, *header, *section_header_entry);
+    if (program_header_entry == NULL) return 1;
+    printf("Found program header which includes the section header: 0x%lx - 0x%lx\n", program_header_entry->p_offset,
+           program_header_entry->p_offset + program_header_entry->p_filesz);
+
+    const Elf64_Phdr *program_header_after_entry = program_header_get_after(file, *header, *program_header_entry);
+    if (program_header_after_entry == NULL) return 1;
+    printf("Found closest program header to the entry one for finding the codecave - starts at 0x%lx\n", program_header_after_entry->p_offset);
 
     code_cave_t code_cave;
-    if (code_cave_get(header, program_header_entry.p_offset, program_header_entry_next.p_offset, &code_cave, file)) {
-        close(file);
-        return 1;
-    }
+    if (code_cave_get(file, &code_cave, *header, program_header_entry->p_offset, program_header_after_entry->p_offset)) return 1;
+    printf("Found biggest code cave from 0x%lx - 0x%lx\n", code_cave.start, code_cave.start + code_cave.size);
 
-    const uint64_t encrytion_start = program_header_entry.p_vaddr + (section_header_entry.sh_offset - program_header_entry.p_offset);
+    const uint64_t encrytion_start = program_header_entry->p_vaddr + (section_header_entry->sh_offset - program_header_entry->p_offset);
 
-    if (shellcode_inject(header, program_header_entry, code_cave, decryption_stub, sizeof(decryption_stub), file, key, encrytion_start,
-                         section_header_entry.sh_size)) {
-        close(file);
-        return 1;
-    }
+    // if (shellcode_inject(*header, *program_header_entry, code_cave, decryption_stub, sizeof(decryption_stub), file, key, encrytion_start,
+    //                      section_header_entry.sh_size)) return 1;
+    //     close(file);
+    //     return 1;
+    // }
 
-    if (section_text_encrypt(section_header_entry, file, key)) {
-        close(file);
-        return 1;
-    }
+    // if (section_text_encrypt(section_header_entry, file, key)) {
+    //     close(file);
+    //     return 1;
+    // }
 
-    close(file);
+    // close(file);
 }
