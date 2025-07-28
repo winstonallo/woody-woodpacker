0e636105e8ced7c66b2518b32396665fccee9815
Arthur Bied-Charreton <arthurbiedchar@gmail.com>
2025-07-28 10:35:11 +0200

    Return error messages from elf64_ident_check

--- src/elf_header.c
+++ src/elf_header.c
@@ -1,25 +1,28 @@
+#include "woody.h"
 #include <assert.h>
 #include <elf.h>
 #include <stdio.h>
 #include <unistd.h>
 
-int
-elf64_ident_check(const Elf64_Ehdr *header) {
-    assert(header);
+static const char *error_messages[] = {
+    "", "Invalid EI_MAG field in header", "Not 64-bit", "Invalid data encoding in header", "Invalid version in header", "Invalid padding in header"};
 
-    if (header->e_ident[EI_MAG0] != ELFMAG0) return 1;
-    if (header->e_ident[EI_MAG1] != ELFMAG1) return 1;
-    if (header->e_ident[EI_MAG2] != ELFMAG2) return 1;
-    if (header->e_ident[EI_MAG3] != ELFMAG3) return 1;
+const char *
+elf64_ident_check(const Elf64_Ehdr *h) {
+    assert(h != NULL);
 
-    if (header->e_ident[EI_CLASS] != ELFCLASS64) return 2;
-
-    if (header->e_ident[EI_DATA] == ELFDATANONE) return 3;
-
-    if (header->e_ident[EI_VERSION] != EV_CURRENT) return 4;
-
-    for (size_t i = EI_PAD; i < sizeof(header->e_ident); i++) {
-        if (header->e_ident[i] != 0) return 5;
+    if (h->e_ident[EI_MAG0] != ELFMAG0 || h->e_ident[EI_MAG1] != ELFMAG1 || h->e_ident[EI_MAG2] != ELFMAG2 || h->e_ident[EI_MAG3] != ELFMAG3) {
+        return error_messages[INVALID_EI_MAG];
     }
-    return 0;
+
+    if (h->e_ident[EI_CLASS] != ELFCLASS64) return error_messages[NOT_64_BIT];
+
+    if (h->e_ident[EI_DATA] == ELFDATANONE) return error_messages[NO_DATA];
+
+    if (h->e_ident[EI_VERSION] != EV_CURRENT) return error_messages[INVALID_VERSION];
+
+    for (size_t i = EI_PAD; i < sizeof(h->e_ident); i++) {
+        if (h->e_ident[i] != 0) return error_messages[INVALID_PADDING];
+    }
+    return error_messages[SUCCESS];
 }
